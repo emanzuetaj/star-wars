c130b30b98b8039455735037fa7fd63662743b56
Elias J Manzueta <eliasmanz@gmail.com>
2020-04-29 01:51:59 -0400

    Force https when necessary

--- client/app/services/swapi/swapi.service.js
+++ client/app/services/swapi/swapi.service.js
@@ -1,8 +1,9 @@
 export default class SwapiService {
-    constructor($q, $http, CacheFactory) {
+    constructor($q, $http, CacheFactory, $location) {
       'ngInject';
       this._$http = $http;
       this._$q = $q;
+      this._$location = $location
       CacheFactory.createCache('starWarsCache', {
         deleteOnExpire: 'aggressive',
         recycleFreq: 60000
@@ -12,6 +13,9 @@ export default class SwapiService {
     // create reusable method, all URLs will be cached anyways
     callApi(apiUrl) {
       const defer = this._$q.defer();
+      if (this._$location.$$protocol === 'https') {
+        apiUrl = apiUrl.replace('http', 'https');
+      }
       this._$http.get(apiUrl, {cache: this.starWarsCache})
         .then((response) => {
           defer.resolve(response.data);
